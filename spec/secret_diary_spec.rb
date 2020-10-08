6a7600edbb86b56ce0eb1840ec5677c45cce3db4
Ben <ben.lynch1931@gmail.com>
2020-10-08 11:18:03 +0100

    Added tests to involve diary double but true instance of SecretDiary

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -49,4 +49,39 @@ RSpec.describe SecretDiary do
       expect(secret_diary.write("Message")).to eq nil
     end
   end
+
+  context "Testing with instance" do
+    let (:diary) { double("diary") }
+
+    context "Works when @unlocked" do
+      before do
+        allow(diary).to receive(:read).and_return("I Read the diary!")
+        allow(diary).to receive(:write).and_return("I wrote to the diary!")
+      end
+
+      it "gets read" do
+        subject = SecretDiary.new(diary)
+        subject.unlock
+        expect(subject.read).to eq("I Read the diary!")
+      end
+      it "gets written" do
+        subject = SecretDiary.new(diary)
+        subject.unlock
+        # expect(subject.write(nil)).to eq("I wrote to the diary!")
+        expect(subject.write(nil)).to eq(nil)
+      end
+    end
+
+    context "Cannot work when @unlocked = false" do
+      before do
+        allow(diary).to receive(:read).and_return("I Read the diary!")
+        allow(diary).to receive(:write).and_return("I wrote to the diary!")
+      end
+      it "returns go away!" do
+        subject = SecretDiary.new(diary)
+        expect(subject.read).to eq "Go away!"
+        expect(subject.write(nil)).to eq "Go away!"
+      end
+    end
+  end
 end
